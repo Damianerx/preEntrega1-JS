b6a112656b7439285f8864dd83f53484c11f7122
Damianerx <damian@erxilape.com.ar>
2024-01-15 23:17:11 -0300

    solucionado el IF, reemplazado por DO WHILE para control de error de ingreso

--- calc.js
+++ calc.js
@@ -1,6 +1,12 @@
-// Declaración de las variables
+let captura;
 
-let captura = prompt("Buenassss...\n\n Qué operación necesitás usar kpo? \n 1 Suma.\n 2 Resta.\n 3 Multiplicación.\n 4 División.\n\n (Presioná la tecla numérica correspondiente a tu selección)");
+do {
+    captura = parseInt(prompt("Buenassss...\n\n Qué operación necesitás usar kpo? \n 1 Suma.\n 2 Resta.\n 3 Multiplicación.\n 4 División.\n\n (Presioná la tecla numérica correspondiente a tu selección)"));
+
+    if (captura < 1 || captura > 4 || isNaN(captura)) {
+        alert("Ingresaste un número inválido.\nIngresá un número del 1 al 4 salamín.");
+    }
+} while (captura < 1 || captura > 4 || isNaN(captura));
 
 let namberUan = parseInt(prompt("Tecleá el primer número: "));
 let namberChu = parseInt(prompt("Tecleá el segundo número: "));
@@ -30,28 +36,23 @@ function div(num1, num2){
 
 //Llamadas a las funciones y resultado
 switch(captura){
+    case 1:
+        resultado = suma(namberUan, namberChu);
+        alert("El resultado de tu SUMA es: " + resultado);
+        break;
 
-    case "1":
-    resultado = suma(namberUan, namberChu);
-    alert("El resultado de tu SUMA es: " + resultado);
-    break;
+    case 2:
+        resultado = resta(namberUan, namberChu);
+        alert("El resultado de tu RESTA es: " + resultado);
+        break;
 
-    case "2":
-    resultado = resta(namberUan, namberChu);
-    alert("El resultado de tu RESTA es: " + resultado);
-    break;
+    case 3:
+        resultado = multi(namberUan, namberChu);
+        alert("El resultado de tu MULTIPLICACIÓN es: " + resultado);
+        break;
 
-    case "3":
-    resultado = multi(namberUan, namberChu);
-    alert("El resultado de tu MULTIPLICACIÓN es: " + resultado);
-    break;        
-
-    case "4":
-    resultado = div(namberUan, namberChu);
-    alert("El resultado de tu SUMA es: " + resultado);
-    break;
-
-    default:
-    alert("Le pifiaste de operación, sólo son válidos los números del 1 al 4");
-    break;
+    case 4:
+        resultado = div(namberUan, namberChu);
+        alert("El resultado de tu DIVISIÓN es: " + resultado);
+        break;
 }
